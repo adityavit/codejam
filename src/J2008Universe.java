6c5ad018bf4e5debf6e011718530e4dad3cc9bcb
Aditya Bhatia <adityavit@gmail.com>
2011-12-05 03:53:34 -0500

    Implementing the 2008 Code jam qualification problem 1

---
+++ src/J2008Universe.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author aditya
+ *
+ */
+public class J2008Universe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
